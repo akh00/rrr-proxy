5a1b8054ed9906e58e0a459878c9b63c5e7cce11
Andrei Kishkevich <>
2025-07-31 19:49:19 +0200

    Add first websocket client impl

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,11 @@ async fn main() {
         .init();
     let proxy_manager = Arc::new(RwLock::<ProxyManager>::new(ProxyManager::new()));
 
-    let load_reporter = ReportLoadSysProvider::new();
-    let register_agent = RegisterAgent::new(
-        "http://localhost:5555".to_string(),
-        500,
-        Box::new(load_reporter),
-    );
+    let load_reporter = Arc::new(RwLock::<ReportLoadSysProvider>::new(
+        ReportLoadSysProvider::new(),
+    ));
+    let register_agent =
+        RegisterAgent::new("http://localhost:5555".to_string(), 500, load_reporter);
     let mut proxy_app = AllocatorService::new(Arc::clone(&proxy_manager));
     try_join!(proxy_app.start(3333), register_agent.run());
 }
--- src/manager/register.rs
+++ src/manager/register.rs
@@ -1,12 +1,16 @@
 use std::error::Error;
+use std::sync::Arc;
+use std::time::Duration;
 
+use futures::channel::mpsc::UnboundedSender;
 use futures::{channel::mpsc, future::select, pin_mut};
 use log::debug;
+use tokio::sync::RwLock;
 use tokio::task::{JoinError, JoinHandle};
+use tokio::time::sleep;
 use tokio_tungstenite::connect_async;
 use tungstenite::Message;
 
-use crate::manager::FixBoxError;
 use crate::manager::load::ReportLoadT;
 
 struct RegisterClient {
@@ -15,11 +19,11 @@ struct RegisterClient {
 }
 
 impl RegisterClient {
-    fn new(url: String) -> Self {
-        RegisterClient { url, timeout: 5000 }
+    fn new(url: String, timeout: u64) -> Self {
+        RegisterClient { url, timeout }
     }
 
-    async fn connect(self, receiver: mpsc::UnboundedReceiver<Message>) -> () {
+    async fn connect(&self, receiver: &mut mpsc::UnboundedReceiver<Message>) -> () {
         use futures::stream::StreamExt;
         match connect_async(&self.url).await {
             Ok((ws_stream, _response)) => {
@@ -46,20 +50,42 @@ impl RegisterClient {
 }
 
 pub struct RegisterAgent {
-    client: RegisterClient,
-    load_reporer: Box<dyn ReportLoadT>,
+    url: String,
+    timeout: u64,
+    load_reporter: Arc<RwLock<dyn ReportLoadT + Send + Sync>>,
 }
 
 impl RegisterAgent {
-    pub fn new(url: String, timeout: u64, load: Box<dyn ReportLoadT>) -> Self {
+    pub fn new(
+        url: String,
+        timeout: u64,
+        load: Arc<RwLock<dyn ReportLoadT + Send + Sync>>,
+    ) -> Self {
         RegisterAgent {
-            client: RegisterClient { url, timeout },
-            load_reporer: load,
+            url: url,
+            timeout: timeout,
+            load_reporter: load,
         }
     }
 
-    pub async fn run(mut self) -> Result<(), &'static str> {
-        let handle = tokio::spawn(async move { () });
+    pub async fn run(self) -> Result<(), &'static str> {
+        let (stdin_tx, mut stdin_rx) = futures_channel::mpsc::unbounded();
+        let handle = tokio::spawn(async move {
+            let client = RegisterClient::new(self.url, self.timeout);
+            loop {
+                tokio::select!(
+                    _ = client.connect(&mut stdin_rx) => {
+                        break;
+                    },
+                    _  = sleep(Duration::from_millis(1000)) => {
+                        if let Ok(load) = self.load_reporter.blocking_write().get_load() {
+
+                        }
+                    }
+                );
+            }
+            ()
+        });
         match handle.await {
             Ok(_) => Ok(()),
             Err(err) => Err("error happened"),
